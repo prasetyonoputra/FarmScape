9d0f2a516d4023ceab5ef551e121bfcc754bf2e2
Prasetyono Putra <prasetyonoptr@gmail.com>
2023-09-10 10:07:05 +0700

    Test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,19 +1,23 @@
-var map = L.map("map").setView([-6.2, 106.816666], 5);
+var map = L.map("map").setView([-2.5, 120], 6);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="http://www.example.com/">Example</a>',
+    // attribution: '&copy; <a href="http://www.example.com/">Example</a>',
     maxNativeZoom: 19,
     maxZoom: 25,
 }).addTo(map);
 
 var drawnItems = new L.FeatureGroup();
 map.addLayer(drawnItems);
+
 var drawControl = new L.Control.Draw({
+    position: "topleft",
     edit: {
         featureGroup: drawnItems,
     },
 });
 map.addControl(drawControl);
+console.log(drawControl);
+// drawControl.container.style.display = "none";
 
 map.on(L.Draw.Event.CREATED, function (e) {
     var type = e.layerType,
@@ -34,4 +38,15 @@ map.on("draw:edited", function (e) {
 
 drawnItems.on("click", (e) => {
     console.log(e);
+
+    let formEdit = document.getElementById("formEdit");
+    formEdit.style.display = "block";
+});
+
+document.getElementById("buttonLadangBaru").addEventListener("click", (e) => {
+    drawControl._toolbars.draw._modes.polygon.handler.enable();
+});
+
+document.getElementById("buttonTitikBaru").addEventListener("click", (e) => {
+    drawControl._toolbars.draw._modes.marker.handler.enable();
 });
